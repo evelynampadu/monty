e886aaac2595f019e1f17624f0e779efd281a192
Evelyn <81477740+evelynampadu@users.noreply.github.com>
2022-06-15 21:12:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,74 +1,152 @@
 #include "monty.h"
-#include "lists.h"
-
-data_t data = DATA_INIT;
+g_t hola;
 
 /**
- * monty - helper function for main function
- * @args: pointer to struct of arguments from main
- *
- * Description: opens and reads from the file
- * containing the opcodes, and calls the function
- * that will find the corresponding executing function
+ * tamanio - size
+ * @linea: linea
+ * Return: linea
  */
-void monty(args_t *args)
+int tamanio(char *linea)
 {
-	size_t len = 0;
-	int get = 0;
-	void (*code_func)(stack_t **, unsigned int);
+	unsigned int contador = 0;
 
-	if (args->ac != 2)
+	while (linea[contador])
 	{
-		dprintf(STDERR_FILENO, USAGE);
-		exit(EXIT_FAILURE);
+		contador++;
 	}
-	data.fptr = fopen(args->av, "r");
-	if (!data.fptr)
-	{
-		dprintf(STDERR_FILENO, FILE_ERROR, args->av);
-		exit(EXIT_FAILURE);
-	}
-	while (1)
-	{
-		args->line_number++;
-		get = getline(&(data.line), &len, data.fptr);
-		if (get < 0)
-			break;
-		data.words = strtow(data.line);
-		if (data.words[0] == NULL || data.words[0][0] == '#')
-		{
-			free_all(0);
-			continue;
-		}
-		code_func = get_func(data.words);
-		if (!code_func)
-		{
-			dprintf(STDERR_FILENO, UNKNOWN, args->line_number, data.words[0]);
-			free_all(1);
-			exit(EXIT_FAILURE);
-		}
-		code_func(&(data.stack), args->line_number);
-		free_all(0);
-	}
-	free_all(1);
+	return (contador);
+}
+
+
+/**
+ * quitarsalto - remove line
+ * @linea: linea
+ * Return: linea
+ */
+char quitarsalto(char *linea)
+{
+	int ta = tamanio(linea);
+
+	linea[ta - 1] = '\0';
+	return (*linea);
 }
 
 /**
- * main - entry point for monty bytecode interpreter
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * pop - delete first noce
+ * @stack: head of the linked list
+ * @num_linea: number of the line
+ * Return: Nothing
  */
-int main(int argc, char *argv[])
+void pop(stack_t **stack, unsigned int num_linea)
 {
-	args_t args;
+	stack_t *ojo;
 
-	args.av = argv[1];
-	args.ac = argc;
-	args.line_number = 0;
+	ojo = *stack;
+	if (ojo == NULL)
+	{
+		dprintf(2, "L%d: can't pop an empty stack\n", num_linea);
+		free(hola.linea);
+		fclose(hola.fil);
+		free_l(stack);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->next)
+	{
+		*stack = ojo->next;
+		(*stack)->prev = NULL;
+		free(ojo);
+	}
+	else
+		free_l(stack);
+}
 
-	monty(&args);
+/**
+ * func - pointers to functions
+ * @tokens: arguments
+ * Return: NULL
+ */
 
-	return (EXIT_SUCCESS);
+void (*func(char *tokens))(stack_t **stack, unsigned int num_linea)
+{
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", divi},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{"stack", stack},
+		{"queue", queue},
+		{"err", err},
+		{"#", nop},
+		{NULL, NULL}
+	};
+	int i = 0;
+	int o;
+
+	while (i < 19)
+	{
+		o = strcmp(ops[i].opcode, tokens);
+		if (o == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (ops[17].f);
+}
+
+/**
+ * main - main function
+ * @argc: number of the arguments
+ * @argv: argument
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+	size_t numbytes = 0;
+	int bytesleidos = 0;
+	unsigned int contador = 1;
+
+	hola.flag = 1;
+	hola.linea = NULL;
+	if (argc != 2)
+	{
+		fputs("USAGE: monty file\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+	hola.fil = fopen(argv[1], "r");
+	if (hola.fil == NULL)
+	{
+		dprintf(2, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while ((bytesleidos = getline(&hola.linea, &numbytes, hola.fil)) != EOF)
+	{
+		quitarsalto(hola.linea);
+		if (hola.linea[0] != 35)
+		{
+			hola.token = strtok(hola.linea, " \t\n");
+			hola.opco = hola.token;
+			if (hola.opco != NULL)
+			{
+				hola.token = strtok(NULL, " \t\n");
+				func(hola.opco)(&stack, contador);
+			}
+			contador++;
+		}
+	}
+	free_l(&stack);
+	free(hola.linea);
+	fclose(hola.fil);
+	return (0);
 }
