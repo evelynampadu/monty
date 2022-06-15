028f51f7e94c777d74bc950fa77862e51e4eeb74
Evelyn <81477740+evelynampadu@users.noreply.github.com>
2022-06-15 21:10:57 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,12 +1,23 @@
-#ifndef MONTY_H
-#define MONTY_H
-
+#ifndef STACK
+#define STACK
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+typedef struct globales
+{
+	int numero;
+	int flag;
+	char *linea;
+	char *token;
+	char *opco;
+	FILE *fil;
+} g_t;
 
+extern g_t hola;
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -37,99 +48,30 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**
- * struct args_s - structure of arguments from main
- * @av: name of the file from the command line
- * @ac: number of arguments from main
- * @line_number: number of the current line in the file
- *
- * Description: arguments passed to main from the command line
- * used in different functions, organized in a struct for clarity
- */
-typedef struct args_s
-{
-	char *av;
-	int ac;
-	unsigned int line_number;
-} args_t;
-
-/**
- * struct data_s - extern data to access inside functions
- * @line: line from the file
- * @words: parsed line
- * @stack: pointer to the stack
- * @fptr: file pointer
- * @qflag: flag for queue or stack
- */
-typedef struct data_s
-{
-	char *line;
-	char **words;
-	stack_t *stack;
-	FILE *fptr;
-	int qflag;
-} data_t;
-
-typedef stack_t dlistint_t;
-
-extern data_t data;
-
-#define DATA_INIT {NULL, NULL, NULL, NULL, 0}
-
-#define USAGE "USAGE: monty file\n"
-#define FILE_ERROR "Error: Can't open file %s\n"
-#define UNKNOWN "L%u: unknown instruction %s\n"
-#define MALLOC_FAIL "Error: malloc failed\n"
-#define PUSH_FAIL "L%u: usage: push integer\n"
-#define PINT_FAIL "L%u: can't pint, stack empty\n"
-#define POP_FAIL "L%u: can't pop an empty stack\n"
-#define SWAP_FAIL "L%u: can't swap, stack too short\n"
-#define ADD_FAIL "L%u: can't add, stack too short\n"
-#define SUB_FAIL "L%u: can't sub, stack too short\n"
-#define DIV_FAIL "L%u: can't div, stack too short\n"
-#define DIV_ZERO "L%u: division by zero\n"
-#define MUL_FAIL "L%u: can't mul, stack too short\n"
-#define MOD_FAIL "L%u: can't mod, stack too short\n"
-#define PCHAR_FAIL "L%u: can't pchar, stack empty\n"
-#define PCHAR_RANGE "L%u: can't pchar, value out of range\n"
-
-/* main.c */
-void monty(args_t *args);
-
-/* get_func.c */
-void (*get_func(char **parsed))(stack_t **, unsigned int);
-void push_handler(stack_t **stack, unsigned int line_number);
-void pall_handler(stack_t **stack, unsigned int line_number);
-
-/* handler_funcs1.c */
-void pint_handler(stack_t **stack, unsigned int line_number);
-void pop_handler(stack_t **stack, unsigned int line_number);
-void swap_handler(stack_t **stack, unsigned int line_number);
-void add_handler(stack_t **stack, unsigned int line_number);
-void nop_handler(stack_t **stack, unsigned int line_number);
-
-/* handler_funcs2.c */
-void sub_handler(stack_t **stack, unsigned int line_number);
-void div_handler(stack_t **stack, unsigned int line_number);
-void mul_handler(stack_t **stack, unsigned int line_number);
-void mod_handler(stack_t **stack, unsigned int line_number);
-
-/* handler_funcs3.c */
-void rotl_handler(stack_t **stack, unsigned int line_number);
-void rotr_handler(stack_t **stack, unsigned int line_number);
-void stack_handler(stack_t **stack, unsigned int line_number);
-void queue_handler(stack_t **stack, unsigned int line_number);
-
-/* char.c */
-void pchar_handler(stack_t **stack, unsigned int line_number);
-void pstr_handler(stack_t **stack, unsigned int line_number);
-
-/* strtow.c */
-int count_word(char *s);
-char **strtow(char *str);
-void free_everything(char **args);
-
-/* free.c */
-void free_all(int all);
+void _mod(stack_t **stack, unsigned int num_linea);
+void pchar(stack_t **stack, unsigned int num_linea);
+void rotr(stack_t **stack, unsigned int num_linea);
+void rotl(stack_t **stack, unsigned int num_linea);
+void stack(stack_t **stack, unsigned int num_linea);
+void queue(stack_t **stack, unsigned int num_linea);
+void pstr(stack_t **stack, unsigned int num_linea);
+void _mul(stack_t **stack, unsigned int num_linea);
+void ver2(stack_t **stack, unsigned int x);
+void err(stack_t **stack, unsigned int x);
+void free_l(stack_t **stack);
+void divi(stack_t **stack, unsigned int num_linea);
+void sub(stack_t **stack, unsigned int num_linea);
+void nop(stack_t **stack, unsigned int num_linea);
+int verif(char *numero);
+int tamanio(char *linea);
+char quitarsalto(char *linea);
+void swap(stack_t **stack, __attribute__((unused)) unsigned int num_linea);
+void add(stack_t **stack, __attribute__((unused)) unsigned int num_linea);
+void pint(stack_t **stack, unsigned int num_linea);
+void pop(stack_t **stack, unsigned int num_linea);
+void (*func(char *tokens))(stack_t **stack, unsigned int num_linea);
+int main(__attribute__((unused)) int argc, char **argv);
+void pall(stack_t **stack, unsigned int num_linea);
+void push(stack_t **stack, unsigned int num_linea);
 
 #endif
